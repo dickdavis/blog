fee5df889e739b1bd1054316fde1129dd9599b38
Dick Davis <rdavis89@my.apsu.edu>
2016-12-19 22:58:15 -0600

    fix for flash

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -17,7 +17,7 @@ class WelcomeController < ApplicationController
     mail = Mail.new(from, subject, to, content)
     sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
     response = sg.client.mail._('send').post(request_body: mail.to_json)
-    if response.status_code == 202
+    if response.status_code == '202'
       flash[:success] = 'Message sent successfully!'
     else
       flash[:danger] = 'Something went wrong. Try again.'
