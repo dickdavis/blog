9fb2746d05979c58916a046d8608de8004abc65d
Dick Davis <rdavis89@my.apsu.edu>
2018-10-31 21:45:15 -0500

    added rss feed route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :projects
   resources :links
   resources :articles
+  get 'feed', to: 'feeds#feed'
   get 'tags/:tag', to: 'articles#index', as: 'tag'
   get 'links' => 'links#index'
   get 'about' => 'welcome#about'
