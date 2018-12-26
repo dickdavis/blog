9cc159a8d84988e5d6f6da4a0e2e4abe125ccdaf
Dick Davis <rdavis89@my.apsu.edu>
2018-12-26 11:55:51 -0600

    fixes security issues with activities and activity categories; refactored activity category controller to conform to standard style

--- test/controllers/activity_categories_controller_test.rb
+++ test/controllers/activity_categories_controller_test.rb
@@ -2,47 +2,48 @@ require 'test_helper'
 
 class ActivityCategoriesControllerTest < ActionDispatch::IntegrationTest
   setup do
+    @user = User.create!(email: 'example@example.com', password: 'letmein')
     @activity_category = activity_categories(:one)
   end
 
   test "should get index" do
-    get activity_categories_url
+    get activity_categories_path(as: @user)
     assert_response :success
   end
 
   test "should get new" do
-    get new_activity_category_url
+    get new_activity_category_path(as: @user)
     assert_response :success
   end
 
   test "should create activity_category" do
     assert_difference('ActivityCategory.count') do
-      post activity_categories_url, params: { activity_category: { description: @activity_category.description, label: @activity_category.label } }
+      post activity_categories_path(as: @user), params: { activity_category: { description: @activity_category.description, label: @activity_category.label } }
     end
 
     assert_redirected_to activity_category_url(ActivityCategory.last)
   end
 
   test "should show activity_category" do
-    get activity_category_url(@activity_category)
+    get activity_category_path(@activity_category, as: @user)
     assert_response :success
   end
 
   test "should get edit" do
-    get edit_activity_category_url(@activity_category)
+    get edit_activity_category_path(@activity_category, as: @user)
     assert_response :success
   end
 
   test "should update activity_category" do
-    patch activity_category_url(@activity_category), params: { activity_category: { description: @activity_category.description, label: @activity_category.label } }
-    assert_redirected_to activity_category_url(@activity_category)
+    patch activity_category_path(@activity_category, as: @user), params: { activity_category: { description: @activity_category.description, label: @activity_category.label } }
+    assert_redirected_to activity_category_path(@activity_category)
   end
 
   test "should destroy activity_category" do
     assert_difference('ActivityCategory.count', -1) do
-      delete activity_category_url(@activity_category)
+      delete activity_category_path(@activity_category, as: @user)
     end
 
-    assert_redirected_to activity_categories_url
+    assert_redirected_to activity_categories_path
   end
 end
